e16e517b8c72b7aff1c2f46a74d4ee3f55e24169
ismail alif <43023388+alif33@users.noreply.github.com>
2024-03-03 17:45:58 +0600

    project changed

--- src/helpers/Http.js
+++ src/helpers/Http.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 import { toast } from "react-hot-toast";
 
-// export const baseURL = "http://localhost:4000/api/";
-export const baseURL = "https://aidyb.vercel.app/api/";
+export const baseURL = "https://dot0notes.vercel.app/api/";
 
 const api = axios.create({
   baseURL
